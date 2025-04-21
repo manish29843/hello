48de2a0234c0f6df547565786f2e64b9732a18e9
manish29843 <miraclemanish3@gmail.com>
2025-04-21 15:29:30 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class hello {
-  public static void main( String arg[]){
-    System.out.println("hello world")
-      }
+ public static void main(String arg[]) {
+   System.out.println("hello");
+ }
 }
